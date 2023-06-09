fc42e5dea4ec13b3bbfd926f2e7c26a4138782bb
Anna Julia Engel <anna.julia.engel@gmail.com>
2023-06-09 18:38:55 +0200

    Add the single DAG

---
+++ dags/load_mysql_data_into_postgres_staging.py
@@ -0,0 +1,10 @@
+from airflow import DAG
+from datetime import datetime
+
+from .tasks import copy_a_single_table
+
+with DAG("load_mysql_data_into_postgres_staging", start_date=datetime(1900, 1, 1), schedule_interval="@once") as dag:
+    copy_a_single_table("CLAIM") >>
+        copy_a_single_table("INVOICE") >>
+        copy_a_single_table("INVOICE_ITEM") >>
+        copy_a_single_table("TREATMENT")
---
+++ dags/tasks/copy_data_to_postgres_staging/__init__.py
@@ -0,0 +1,1 @@
+from .copy_a_single_table import copy_a_single_table 
