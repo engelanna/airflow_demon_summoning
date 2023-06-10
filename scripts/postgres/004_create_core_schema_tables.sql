62f0cd4409150eb6411820fda8979053787b2e55
Anna Julia Engel <anna.julia.engel@gmail.com>
2023-06-10 07:29:43 +0200

    Restore column order in staging to match MySQL's

--- scripts/postgres/004_create_core_schema_tables.sql
+++ scripts/postgres/004_create_core_schema_tables.sql
@@ -1,40 +1,40 @@
-create table core.claim (
-    amount decimal(19,4),
-    currency varchar(4),
-    date_created timestamp,
-    date_modified timestamp,
+create table core.invoice_item (
     id int primary key,
-    program_id int,
     status varchar(25),
-    treatment_id int,
-    type varchar(25)
+    date_created timestamp,
+    invoice_id int,
+    product_id int,
+    currency varchar(4),
+    amount decimal(19,4)
 );
 
 create table core.invoice (
-    date_created timestamp,
     id int primary key,
     status varchar(25),
+    date_created timestamp,
     treatment_id int
 );
 
-create table core.invoice_item (
-    amount decimal(19,4),
-    currency varchar(4),
-    date_created timestamp,
+create table core.claim (
     id int primary key,
-    invoice_id int,
-    product_id int,
-    status varchar(25)
+    status varchar(25),
+    type varchar(25),
+    date_created timestamp,
+    date_modified timestamp,
+    treatment_id int,
+    program_id int,
+    amount decimal(19,4),
+    currency varchar(4)
 );
 
 create table core.treatment (
+    id int primary key,
+    status varchar(25),
+    type varchar(25),
     date_created timestamp,
     date_modified timestamp,
     date_submitted timestamp, 
     date_treatment timestamp,
-    id int primary key,
-    program_id int,
     provider_id int,
-    status varchar(25),
-    type varchar(25)
+    program_id int
 );
