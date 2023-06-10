e596473d97964e55d6170e2f1fbd87bf88ec65b4
Anna Julia Engel <anna.julia.engel@gmail.com>
2023-06-10 06:24:27 +0200

    Add fact tables (atomic & bridge) to analytics schema

--- scripts/postgres/005_create_analytics_schema_standard_dimensions.sql
+++ scripts/postgres/005_create_analytics_schema_standard_dimensions.sql
@@ -3,6 +3,8 @@
     That includes degenerate dimensions: program, provider.
 */
 
+/*
+
 create table analytics.dim_claim (
     currency varchar(4),
     date_created timestamp,
@@ -24,20 +26,35 @@ create table analytics.dim_invoice_item (
 );
 
 create table analytics.dim_program (
+    claims_last_7_days default 0,
+    claims_last_28_days default 0,
+    claims_total default 0,
     id int primary key,
+    treatments_last_7_days default 0,
+    treatments_last_28_days default 0,
+    treatments_total default 0,
 );
 
 create table analytics.dim_provider (
     id int primary key,
+    treatments_last_7_days default 0, 
+    treatments_last_28_days default 0,
+    treatments_total default 0,
 );
 
 create table analytics.dim_treatment (
-    claims_last_7_days  default 0,
-    claims_last_28_days  default 0,
+    claims_last_7_days default 0,
+    claims_last_28_days default 0,
+    claims_total default 0,
     date_created timestamp,  
     date_modified timestamp,
     date_submitted timestamp,  
     date_treatment timestamp,  
     id int primary key,
-    type varchar(25)
+    invoices_last_7_days default 0,
+    invoices_last_28_days default 0,
+    invoices_total default 0,
+    type varchar(25),
 );
+
+*/
---
+++ scripts/postgres/007_create_analytics_schema_bridge_facts.sql
@@ -0,0 +1,41 @@
+/* 
+    Statuses that change across time, and slice across dimensions.
+*/
+
+/*
+
+create table analytics.fct_claim_bridge (
+    activity_date timestamp,
+    activity_status varchar(25),
+    amount decimal(19,4),
+    dim_claim_id int,
+    dim_program_id int,
+    dim_treatment_id int,
+);
+
+create table analytics.fct_invoice_bridge (
+    activity_date timestamp,
+    activity_status varchar(25),
+    dim_invoice_id int,
+    dim_treatment_id int,
+);
+
+create table analytics.fct_invoice_item_bridge (
+    activity_date timestamp,
+    activity_status varchar(25),
+    amount decimal(19,4),
+    currency varchar(4),
+    dim_invoice_id int,
+    dim_invoice_item_id int,
+    dim_product_id int,
+);
+
+create table analytics.fct_treatment_bridge (
+    activity_date timestamp,
+    activity_status varchar(25),
+    dim_program id int,
+    dim_provider_id int,
+    dim_treatment id int,
+);
+
+*/
